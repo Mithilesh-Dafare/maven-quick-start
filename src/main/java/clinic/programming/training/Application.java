89ef4a18d828bc3705894b60647c6c1de9f495ab
Mithilesh Dafare <mithileshdafare137@gmail.com>
2026-05-05 09:53:39 +0530

    Adding

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -21,8 +21,8 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+        System.out.println ("Starting Application");
+        Application app = new Application();
 	app.greet();
 
     }
